c1a9fe194e0d8776821c998e3e7eb8236af0c13e
geoffreycc <gcox3d@gmail.com>
2016-05-21 13:08:14 -0700

    User Array is obtained from local storage or created and sent to local storage.

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ formEl.addEventListener('submit', function(event) {
   var userName = event.target.name.value;
   console.log('username = ' + userName);
 });
+
+// JSON.stringify(-)
+// localStorage.setItem(key, value)
+//
+// localStorage.getItem(key)
+// JSON.parse(-)
+//
+
+
+if (localStorage.getItem('userAccount') === null) { //This may not be neccessary to do.
+  console.log("userAccount array created.");
+  var userAccount = [];
+  localStorage.setItem("userAccount", JSON.stringify(userAccount));
+} else {
+  var userAccount = JSON.parse(localStorage.getItem('userAccount'));
+  console.log("userAccount obtained from localStorage.");
+}
+
+
+// var userAcount = [];
