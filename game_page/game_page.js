d432d8b7efcecf914c0ff63f158cbce8f5351b23
Frank Iriarte <jurrish@gmail.com>
2016-05-21 12:42:56 -0700

    picture populating into html, need to connect answers to individual questions

--- game_page/game_page.js
+++ game_page/game_page.js
@@ -1,1 +1,23 @@
+var questionsArray = [];
+var questionNames = ['calculate', 'callQuestion', 'htmlQuestion', 'testQuestion', 'varQuestion'];
 
+function Question(name, path) {
+  this.name = name;
+  this.path = path;
+  questionsArray.push(this);
+}
+
+(function addingPhotos() {
+  for (var i = 0; i < questionNames.length; i++) {
+    new Question(questionNames[i], 'questions/' + questionNames[i] + '.png');
+  }
+})();
+
+var questionObject = {
+  elObj : document.getElementById('img'),
+
+  appendingImage: function() {
+    this.elObj.src = questionsArray[0].path;
+  }
+};
+questionObject.appendingImage();
