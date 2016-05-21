465936289202dc56629e90f592a9ee7492d8575f
Frank Iriarte <jurrish@gmail.com>
2016-05-21 09:48:19 -0700

    html of first page set

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 
+var formEl = document.getElementById('form');
+
+formEl.addEventListener('submit', function(event) {
+  event.preventDefault();
+  var userName = event.target.submit.value;
+  console.log('username = ' + userName);
+});
