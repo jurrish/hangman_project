28614007e36f93805e009032446d198c946bbbc2
Steve Bachtel <sbachtel@gmail.com>
2016-05-21 13:30:06 -0700

    made the radio buttons

--- game_page/game_page.js
+++ game_page/game_page.js
@@ -1,1 +1,25 @@
+var questionsArray = [];
+var questionNames = ['calculate', 'callQuestion', 'htmlQuestion', 'testQuestion', 'varQuestion'];
 
+function Question(name, path) {
+ this.name = name;
+ this.path = path;
+ questionsArray.push(this);
+}
+
+(function addingPhotos() {
+ for (var i = 0; i < questionNames.length; i++) {
+   new Question(questionNames[i], 'game_page/questions/' + questionNames[i] + '.png');
+ }
+})();
+
+var questionObject = {
+ elObj : document.getElementById('question'),
+ imgOne : document.createElement('img'),
+
+ appendingImage: function() {
+   var questionPic = questionObject.elObj.appendChild(questionObject.imgOne);//changed addingPhotos.elObj to this.elObj. But is that correct? AND WHY??
+   this.questionPic.src = questionsArray[0].path;
+ }
+};
+questionObject.appendingImage();
