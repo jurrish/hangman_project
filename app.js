49cf38b87be2e86f47174e295eda48cecf6d3c85
Frank Iriarte <jurrish@gmail.com>
2016-05-21 11:04:48 -0700

    added questions pictures

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var formEl = document.getElementById('form');
 
 formEl.addEventListener('submit', function(event) {
   event.preventDefault();
-  var userName = event.target.submit.value;
+  var userName = event.target.name.value;
   console.log('username = ' + userName);
 });
