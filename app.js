125b5a2ec50bf8cdb08f30ba7c24cb7a9c8dd874
geoffreycc <gcox3d@gmail.com>
2016-05-21 17:28:26 -0700

    Input features appear to be working. Not updating localStorage.

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ if (localStorage.getItem('userAccount') === null) { //This may not be neccessary
 } else {
   var userAccountArray = JSON.parse(localStorage.getItem('userAccount'));
   console.log("userAccount obtained from localStorage.");
-}
+}                                                                                     //WORKING!!!!!!!
 
 var formEl = document.getElementById('form'); //Controls the userName and passWord inputs
 
@@ -28,31 +28,50 @@ function UserAccount(userName, passWord) {
   this.passWord = passWord;
   this.questionsAsked = 0;
   userAccountArray.push(this);
-  console.log("New user created! UserAccount object constructor was used.")
+  console.log("New user created! UserAccount object constructor was used.")          //WORKING!!!!!!!!!!!
 }
 
 var accountFunctions = {                                                            //It appears that extra objects area created and added to the array.
-  activeUser : "",
+  activeUser : null,
   findUser : function(userName, passWord) {
-    if (userAccountArray.length > 0) {
-      for (users in userAccountArray) {
-        if ((userName === userAccountArray[users].userName && passWord) === (userAccountArray[users].passWord)) {
-          activeUser = userAccountArray[users];
-          break;
-          console.log(activeUser + ' already exists and is the current user.');
-        } else {
-          activeUser = new UserAccount(userName, passWord);
-          console.log(activeUser);
-          localStorage.setItem( "userAccount", JSON.stringify(userAccountArray)); // Happening during the for loop when results don't match. Not good. Maybe something with null?
+    for (users in userAccountArray) {
+      if (userName === userAccountArray[users].userName) {
+        for (users in userAccountArray) {
+          if (passWord === userAccountArray[users].passWord) {
+            console.log('innerLoop')
+            accountFunctions.activeUser = userAccountArray[users];
+            console.log('findUser method found the user account')
+          }
         }
       }
-    } else {
-      activeUser = new UserAccount(userName, passWord);
-      console.log(activeUser);
-      localStorage.setItem( "userAccount", JSON.stringify(userAccountArray));
+    }                                                                                    //WORKING!!!
+  },
 
+  newUser : function(obj, userName, passWord) {
+    if (obj === null) {
+      accountFunctions.activeUser = new UserAccount(userName, passWord);
+      console.log("newUser method used to create a new user account");
+      console.log(accountFunctions.activeUser);
     }
-  }
+  },
+  //   if (userAccountArray.length > 0) {
+  //     for (users in userAccountArray) {
+  //       if ((userName === userAccountArray[users].userName && passWord) === (userAccountArray[users].passWord)) {
+  //         activeUser = userAccountArray[users];
+  //         break;
+  //         console.log(activeUser + ' already exists and is the current user.');
+  //       } else {
+  //         activeUser = new UserAccount(userName, passWord);
+  //         console.log(activeUser);
+  //         localStorage.setItem( "userAccount", JSON.stringify(userAccountArray)); // Happening during the for loop when results don't match. Not good. Maybe something with null?
+  //       }
+  //     }
+  //   } else {
+  //     activeUser = new UserAccount(userName, passWord);
+  //     console.log(activeUser);
+  //     localStorage.setItem( "userAccount", JSON.stringify(userAccountArray));
+  //
+  //   }
 }
 
 formEl.addEventListener('submit', function(event) {
@@ -61,5 +80,6 @@ formEl.addEventListener('submit', function(event) {
   var passWordInput = event.target.password.value;
   console.log('username = ' + userNameInput + '  ' + passWordInput + ' is the password.');
   accountFunctions.findUser(userNameInput, passWordInput);
+  accountFunctions.newUser(accountFunctions.activeUser, userNameInput, passWordInput);
 
 });
