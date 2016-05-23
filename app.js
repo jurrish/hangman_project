0361f541187e8a7af655898ea6e29a46f9c5c440
geoffreycc <gcox3d@gmail.com>
2016-05-23 13:40:01 -0700

    Added eslint file and fixed issues lint found.

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 
-
 var questionsArray = [];
 var questionNames = ['calculate', 'callQuestion', 'htmlQuestion', 'testQuestion', 'varQuestion'];
 
-
 function Question(name, path) {
- this.name = name;
- this.path = path;
- questionsArray.push(this);
+  this.name = name;
+  this.path = path;
+  questionsArray.push(this);
 }
 
 //New Code is below this comment.
 
 if (localStorage.getItem('userAccount') === null) { //This may not be neccessary to do. On page load either get or make questionsArray
-  console.log("userAccount array created.");
+  console.log('userAccount array created.');
   var userAccountArray = [];
-  localStorage.setItem("userAccount", JSON.stringify(userAccountArray));
+  localStorage.setItem('userAccount', JSON.stringify(userAccountArray));
 } else {
   var userAccountArray = JSON.parse(localStorage.getItem('userAccount'));
-  console.log("userAccount obtained from localStorage.");
+  console.log('userAccount obtained from localStorage.');
 }                                                                                     //WORKING!!!!!!!
 
 var formEl = document.getElementById('form'); //Controls the userName and passWord inputs
@@ -28,7 +26,7 @@ function UserAccount(userName, passWord) {
   this.passWord = passWord;
   this.questionsAsked = 0;
   userAccountArray.push(this);
-  console.log("New user created! UserAccount object constructor was used.")          //WORKING!!!!!!!!!!!
+  console.log('New user created! UserAccount object constructor was used.');          //WORKING!!!!!!!!!!!
 }
 
 var accountFunctions = {                                                            //It appears that extra objects area created and added to the array.
@@ -38,9 +36,9 @@ var accountFunctions = {                                                        
       if (userName === userAccountArray[users].userName) {
         for (users in userAccountArray) {
           if (passWord === userAccountArray[users].passWord) {
-            console.log('innerLoop')
+            console.log('innerLoop');
             accountFunctions.activeUser = userAccountArray[users];
-            console.log('findUser method found the user account')
+            console.log('findUser method found the user account');
           }
         }
       }
@@ -50,11 +48,22 @@ var accountFunctions = {                                                        
   newUser : function(obj, userName, passWord) {
     if (obj === null) {
       accountFunctions.activeUser = new UserAccount(userName, passWord);
-      console.log("newUser method used to create a new user account");
+      console.log('newUser method used to create a new user account');
       console.log(accountFunctions.activeUser);
-      localStorage.setItem("userAccount", JSON.stringify(userAccountArray));
+      localStorage.setItem('userAccount', JSON.stringify(userAccountArray));
     }
   },
+
+  indexOnSubmit : function() {
+    event.preventDefault();
+    var userNameInput = event.target.name.value;
+    var passWordInput = event.target.password.value;
+    accountFunctions.findUser(userNameInput, passWordInput);
+    accountFunctions.newUser(accountFunctions.activeUser, userNameInput, passWordInput);
+    window.location = 'game_page/game_page.html';
+    formEl.removeEventListener('submit', indexOnSubmit); // Double check the format for this.
+  }
+};
   //   if (userAccountArray.length > 0) {
   //     for (users in userAccountArray) {
   //       if ((userName === userAccountArray[users].userName && passWord) === (userAccountArray[users].passWord)) {
@@ -73,7 +82,6 @@ var accountFunctions = {                                                        
   //     localStorage.setItem( "userAccount", JSON.stringify(userAccountArray));
   //
   //   }
-}
 
 formEl.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -82,5 +90,5 @@ formEl.addEventListener('submit', function(event) {
   console.log('username = ' + userNameInput + '  ' + passWordInput + ' is the password.');
   accountFunctions.findUser(userNameInput, passWordInput);
   accountFunctions.newUser(accountFunctions.activeUser, userNameInput, passWordInput);
-
+  window.location = 'game_page/game_page.html';
 });
