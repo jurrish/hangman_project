6ed9e2f4ba3e6dd9a37bc801f2a78f7ab69b9488
Frank Iriarte <jurrish@gmail.com>
2016-05-23 18:48:35 -0700

    added some ids to html so js could grab and change values for our questions

--- game_page/game_page.js
+++ game_page/game_page.js
@@ -1,7 +1,8 @@
 var questionsArray = [];
 var answerArray = [];
 var questionNames = ['calculate', 'callQuestion', 'htmlQuestion', 'testQuestion', 'varQuestion', 'childQuestion', 'methodQuestion', 'methodQuestion2', 'multipleObjects', 'objLitQuestion', 'returnQuestion', 'roundDownQuestion'];
-var correctAnswers = [];
+var correctAnswers = ['15'];
+var totalAnswers = ['15', '40', '8', 'area'];
 
 function Question(name, path) {
   this.name = name;
@@ -16,13 +17,46 @@ function Question(name, path) {
 })();
 
 var questionObject = {
-  elObj : document.getElementById('img'),
+  elObj: document.getElementById('img'),
   elQuestion: document.createElement('p'),
   elTest: document.getElementById('question'),
-
+  elAnswer: document.getElementById('test'),
+  elOptionTwo: document.getElementById('testTwo'),
+  elOptionThree: document.getElementById('testThree'),
+  elOptionFour: document.getElementById('testFour'),
+  // document.getElementById('test').textContent = 'Boop' - this works, but we need it as a method(maybe even a prototype to be applied to all instances of the question object)
   appendingImage: function() {
     this.elObj.src = questionsArray[0].path;
     this.elTest.textContent = 'What is the value of wallOne?';
+  },
+  appendingAnswer: function() {//this doesn't work, but we need it as  method or a prototype!
+    this.elAnswer.textContent = 'option1';
+    this.elOptionTwo.textContent = 'option2';
+    this.elOptionThree.textContent = 'option3';
+    this.elOptionFour.textContent = 'option4';
   }
 };
+
 questionObject.appendingImage();
+questionObject.appendingAnswer();
+
+// var answerObject = {
+//   answerOne: document.getElementById('test').label,
+//
+//   getRandomIndex: function() {
+//     return Math.floor(Math.random() * totalAnswers.length);
+//   },
+//
+//   displayAnswers: function() {
+//     if(correctAnswers[i] === totalAnswers[i]){
+//       this.displayAnswers();
+//       console.log('you are correct');
+//     } else {
+//       console.log('you are incorrect');
+//     }
+//   }
+//   for(var i = 0; i < totalAnswers.length; i++) {
+//
+//     }
+// };
+// answerObject.displayAnswers();
